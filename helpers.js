cd7c99c6bc1ba03972bc9ad19a88e361cffe1292
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2020-02-07 17:41:36 +0300

    CL-592: Initial commit

---
+++ helpers.js
@@ -0,0 +1,28 @@
+// Make requests to CryptoCompare API
+export async function makeApiRequest(path) {
+	try {
+	  const response = await fetch(`https://min-api.cryptocompare.com/${path}`);
+	  return response.json();
+  } catch (error) {
+    throw new Error(`CryptoCompare request error: ${error.status}`);
+	}
+}
+  
+  // Generate a symbol ID from a pair of the coins
+  export function generateSymbol(exchange, fromSymbol, toSymbol) {
+	const short = `${fromSymbol}/${toSymbol}`;
+	return {
+	  short,
+	  full: `${exchange}:${short}`,
+	};
+}
+
+export function parseFullSymbol(fullSymbol) {
+  const match = fullSymbol.match(/^(\w+):(\w+)\/(\w+)$/);
+  if (!match) {
+    return null;
+  }
+
+  return { exchange: match[1], fromSymbol: match[2], toSymbol: match[3] };
+}
+  
---
+++ main.js
@@ -0,0 +1,11 @@
+// Datafeed implementation, will be added later
+import Datafeed from './datafeed.js';
+
+window.tvWidget = new TradingView.widget({
+  symbol: 'Bitfinex:BTC/USD', // default symbol
+  interval: '1D', // default interval
+  fullscreen: true, // displays the chart in the fullscreen mode
+  container_id: 'tv_chart_container',
+  datafeed: Datafeed,
+  library_path: 'charting_library_clonned_data/charting_library/',
+});
